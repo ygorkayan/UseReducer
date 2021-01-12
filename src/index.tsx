68e263104f3025d3f9969cf0cf070fa4e08c38a4
ygor kayan <ygorkayan@gmail.com>
2021-01-12 13:18:05 -0300

    inicio

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UseReducer from "./UseReducer";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UseReducer />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
